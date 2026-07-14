cfdc8482d7ade1712217d0f2a964f9e6d4cae53f
Syed Fahad <syedf8707@gmail.com>
2026-07-14 19:47:56 +0500

    initial push

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from "react-router"
+import Header from "./components/Header"
+import NewsPage from "./components/NewsPage"
+import Sidebar from "./components/Sidebar"
+import SavedNews from "./components/SavedNews"
+import TopHeadline from "./components/TopHeadline"
+import NewsDetail from "./components/NewsDetail"
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen bg-[#0F172A] text-slate-200 font-sans">
+        <TopHeadline/>
+        <div className="max-w-400 mx-auto flex min-h-screen">
+          <Sidebar/>
+          <main className="flex-1 md:ml-20 lg:ml-64 p-4 lg:p-8">
+            <Header/>
+            <Routes>
+              <Route path="/" element={<NewsPage/>}/>
+              <Route path="/saved-news" element={<SavedNews/>}/>
+              <Route path="/news-detail/:id" element={<NewsDetail/>}/>
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
